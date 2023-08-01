14adeebb2066227fa6260712838035f226aa0529
Jayne Valverde <jaynevalverde@gmail.com>
2023-08-01 14:35:55 -0500

    handle bar main set up

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const PORT = process.env.PORT || 3001;
 
 // Set up Hanldebars as the view engine 
 const hbs = exphbs.create({ helpers });
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -20,10 +22,15 @@ app.set('view engine', 'handlebars');
 // Testing purposes 
 // Define the new route
 app.get("/", (req, res) => {
-  // res.send("<h2>HELLLOOOO WOOOOORLD!</h1>")
+
     // Render the home template
-    res.render('home'); // Assuming you have a template engine like Handlebars set up
+    res.render('./layouts/main'); // Assuming you have a template engine like Handlebars set up
   });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
